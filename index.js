b832fdfcf83be7139e0ace7e252c6354cb0ab73c
aeleby <ae.mae.aw@gmail.com>
2023-02-26 16:18:47 -0800

    edit so that the grid is dynamic in sizing

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const sizeOfGrid = 16;
 const resetButton = document.querySelector("button");
 document.querySelector(".bigContainer");
 const container = document.querySelector(".bigContainer");
+
 const createGrid = (amtofGrids) => {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("wrapper");
   for (let i = 0; i < amtofGrids; i++) {
     const row = document.createElement("div");
     row.classList.add("grid-row");
@@ -16,8 +19,22 @@ const createGrid = (amtofGrids) => {
       });
       row.appendChild(gridBox);
     }
-    container.appendChild(row);
+    wrapper.appendChild(row);
   }
+  container.appendChild(wrapper);
 };
 
 createGrid(sizeOfGrid);
+resetButton.addEventListener("click", () => {
+  let userSize = Number(prompt("choose a size between 1-100"));
+  while (userSize > 100) {
+    const userSize = Number(prompt("choose a size between 1-100"));
+  }
+  const wrapper = document.querySelector(".wrapper");
+  if (!wrapper) {
+    createGrid(userSize);
+  } else {
+    wrapper.remove();
+    createGrid(userSize);
+  }
+});
